e3fc60159077dcab3166e772db751a7250197298
Sevada <64130935+Sevada797@users.noreply.github.com>
2023-12-18 08:25:07 +0400

    Update easyLoopLimiter.js

--- Scripts/easyLoopLimiter.js
+++ Scripts/easyLoopLimiter.js
@@ -1,19 +1,20 @@
 /////////////////////////
 //////    NOTE    ///////
 /////////////////////////
-// Any of these funcitons can be used there is a small difference between them tho
+// Any of these two functions can be used, but there is a small difference between them tho
+// When using first one, let's say you do alert() each time, whenever you close the alert than the timer will continue,
+// meanwhile the second script won't care, so if you press alert late intentanoally you will see next alert poping up at light speed
+// but tbh I don't know in what any other situations this could cause trouble or advantage over one another, so I will just keep both
 //
-//
-//
-///////////////////////
-/// example scripts ///
-///////////////////////
-//  /* This will generate all 4 digit numbers and log them */
+/////////////////////////
+///  example scripts  ///
+/////////////////////////
+//  /* This will generate all 4 digit numbers and log them with pause of 0.2 seconds each time */
 // easyLoopLimiter( 0, 10000, 200, "let a = (i - ( i % 1000 ) ) / 1000; let b = ( i - ( i % 100 ) ) / 100; let c = ( i - ( i % 10) ) / 10; let d = i % 10; console.log( a.toString() + b.toString() + c.toString() + d.toString() )" )
 //
-// /* Or you can have your custom function maybe for making a fetch requests continously or something custom */ 
+// /* Or you can have your function maybe for making a fetch requests continously or something custom? */ 
 // easyLoopLimiter( 0, 1000, 300, 'yourCustomFunction()')
-//
+//  NOTE:  Please also note that in your custom function you can use variable `i` as it will be accessible
 //
 
 
