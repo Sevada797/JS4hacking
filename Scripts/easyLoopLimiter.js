2df8fb4280a7c45ea871600b177018bcba0033df
Sevada <64130935+Sevada797@users.noreply.github.com>
2023-12-18 06:43:57 +0400

    Update easyLoopLimiter.js

--- Scripts/easyLoopLimiter.js
+++ Scripts/easyLoopLimiter.js
@@ -1,4 +1,4 @@
-//Any of these funcitons can be used there is a small difference between them tho
+// Any of these funcitons can be used there is a small difference between them tho
 //
 //
 //
@@ -6,8 +6,10 @@
 /// example scripts ///
 ///////////////////////
 //  /* This will generate all 4 digit numbers and log them */
-// easyLoopLimiter(0, 10000, 200, "let a=(i-(i%1000))/1000; let b=(i-(i%100))/100; let c=(i-(i%10))/10; let d=i%10; console.log(a.toString()+b.toString()+c.toString()+d.toString() )")
-//
+// easyLoopLimiter( 0, 10000, 200, "let a = (i - ( i % 1000 ) ) / 1000; let b = ( i - ( i % 100 ) ) / 100; let c = ( i - ( i % 10) ) / 10; let d = i % 10; console.log( a.toString() + b.toString() + c.toString() + d.toString() )" )
+// /* Or you can have your custom function maybe for making a fetch requests continously or something custom */ 
+// easyLoopLimiter( 0, 1000, 300, 'yourCustomFunction()')
+
 function easyLoopLimiter(loopStrtNum, loopEndNum, timeToPause, yourFunction) {
 
     function loop(i) {
