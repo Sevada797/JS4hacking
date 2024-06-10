afb26a49996e83f5a77ff2cf4493ed92c2c77e11
Sevada <64130935+Sevada797@users.noreply.github.com>
2024-06-10 16:31:24 +0400

    Create clickjackx3f.js

---
+++ Scripts/clickjackx3f.js
@@ -0,0 +1,47 @@
+ const bypassed = [];
+    const iframe = document.createElement('iframe');
+    document.body.appendChild(iframe);
+// easyLoopLimiter function
+function easyLoopLimiter(loopStrtNum, loopEndNum, timeToPause, yourFunction) {
+
+    function loop(i) {
+        if (i < loopEndNum) {
+            eval(yourFunction);
+            setTimeout(() => {
+                i++;
+                loop(i)
+            }, timeToPause);
+        } else {
+            return;
+        }
+    }
+
+    setTimeout(() => {
+        loop(loopStrtNum)
+    }, timeToPause);
+
+}
+
+    function loadUrl(i) {
+        if (i < urls.length) {
+            iframe.src = urls[i];
+            iframe.onload = function() {
+                bypassed.push(urls[i]);
+                console.log(`URL bypassed: ${urls[i]}`);
+            };
+        }
+    }
+
+
+// Get input list and split by newline
+const inputList = `https://example.com
+https://facebook.com/
+https://chatgpt.com/backend-api/me
+`;
+
+// Convert input list to an array of URLs
+const urls = inputList.split('\n');
+    // Start the loop using easyLoopLimiter
+    easyLoopLimiter(0, urls.length, 200, 'loadUrl(i)');
+
+
