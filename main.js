62dab8999288ecaaf6d59231b9c53ed0df8f6ab3
Sevada <sevada@Someofmydomains.com>
2026-01-13 14:23:09 +0400

    fix for links() function

--- main.js
+++ main.js
@@ -552,18 +552,10 @@ function showhtml() {
 let a=prompt(); document.body.innerHTML=a;
 }
 function links() {
-// Define a regular expression to match URLs
-const urlRegex = /\bhttps?:\/\/[^\s/$.?#].[^\s]*\b/g;
-
-// Get the text content of the document
-const documentText = document.body.innerHTML;
-
-// Use the regular expression to find all URLs in the document
-const urls = documentText.match(urlRegex);
-
-// Log the extracted URLs to the console
-return urls;
-
+    const urlRegex = /https?:\/\/[^\s"'\<\>]+/g; // simpler & safer
+    const documentText = document.body.innerHTML;
+    const urls = documentText.match(urlRegex);
+    return urls;
 }
 async function lql() {
     // --- Payloads list ---
